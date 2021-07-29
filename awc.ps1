1de3d9d4022807a552a7c8b1786f05c8fbd145df
trigger190629 <88093732+trigger190629@users.noreply.github.com>
2021-07-29 09:45:36 -0400

    Add files via upload

---
+++ awc.ps1
@@ -0,0 +1,6 @@
+﻿$apiKey = "64c6e311b28f4e82b486becc16cfe368"
+$apiURL = "https://www.bungie.net/Platform"
+$bungieNetID = "14747722"
+
+
+$response = Invoke-RestMethod -Method Get -Uri "$apiURL/User/GetBungieNetUserById/$bungieNetID" -Headers @{"X-API-Key" = $apiKey}
